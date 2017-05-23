64e877f05d31cff8219d28afe72395118891ea53
MrFlashf <krz.jozefowicz@gmail.com>
2017-05-23 19:05:20 +0200

    Add graphics

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <random>
-#include <stdlib.h>
-#include <time.h>
-#include <cstdlib>
+//#include <stdlib.h>
+//#include <time.h>
+//#include <cstdlib>
 #include <tgmath.h>
-//#include <graphics.h>
+#include<graphics.h>
+
 using namespace std;
-const int SIZE = 50;
+const int SIZE          = 50;
+const int START_X       = 20;
+const int START_Y       = 20;
+const int PIXEL_SIZE    = 5;
 
 void randomizeArray(double array[SIZE][SIZE]) {
     for (int i = 0; i < SIZE; i++) {
@@ -25,6 +29,7 @@ private:
     double temperature;
 public:
     Model(int steps, double temperature) {
+        srand(time(0));
         this->steps = steps;
         this->temperature = temperature;
         randomizeArray(array);
@@ -35,9 +40,7 @@ public:
         array[y][x] *= -1.0;
     }
 
-    void touch() {
-        int x = rand() % SIZE;
-        int y = rand() % SIZE;
+    void touch(int x,int y) {
 
         double neighbours[4] = {
             array[y][x + 1 == SIZE ? 0 : x + 1], //right
@@ -53,33 +56,34 @@ public:
             neighboursSum += neighbours[i];
         }
 
-        double ep = -1 * state * neighboursSum;
-        double ek =      state * neighboursSum;
+        double ek = state * neighboursSum;
+        double ep = -1 * ek;
 
         double deltaE   = ek - ep;
         double t        = -1 * (deltaE / temperature);
         double exp1     = exp(t);
         double r        = ((double)rand() / (double)RAND_MAX);
 
-        if (deltaE <= 0 || r <= exp1) {
+        if ((deltaE <= 0) || r <= exp1) {
             invert(x,y);
         }
     }
 
-    void display() {
+    void draw() {
+        int start = 100;
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                string toDisplay = array[i][j] == 1 ? "x" : "o";
-                cout << toDisplay;
+                array[i][j] == -1 ? setcolor(15) : setcolor(3);
+                bar(START_X+j*PIXEL_SIZE,START_Y+i*PIXEL_SIZE,START_X+(j+1)*PIXEL_SIZE,START_Y+(i+1)*PIXEL_SIZE);
             }
-            cout << endl;
         }
-        cout << endl<<endl;
     }
 
     void mcStep() {
         for (int i = 0; i < SIZE*SIZE; i++) {
-            touch();
+            int x = rand() % SIZE;
+            int y = rand() % SIZE;
+            touch(x,y);
         }
     }
 
@@ -91,28 +95,41 @@ public:
             }
         }
         m = m / (SIZE*SIZE);
-        return m == 1 || m == -1;
+        m < 0 ? m = -1*m : m;
+        cout << m << endl;
+        return m == 1;
     }
 
     void run() {
-//        display();
         for (int i = 0; i < steps; i++) {
             mcStep();
             stepsDone++;
-            if (i % 5000==0)
-                display();
+            if (i % 100==0)
+                draw();
             if (sum()) {
                 cout << stepsDone;
-                display();
                 return;
             }
         }
     }
 };
 
-int main() {
+int main(int argc, char** argv) {
+    int gd = DETECT,gm;
+    initgraph(&gd,&gm,NULL);
     srand(time(0));
-    new Model(6000000, 2);
+
+    int mcsteps = argc > 2 ? stoi(argv[1]) : 50000;
+    int temp    = argc > 2 ? stoi(argv[2]) : 1;
+//    cout << mcsteps << ", " << temp << endl;
+    new Model(mcsteps, temp);
+//    lineto(100,200);
+//    new Model(1, 1);
+//    delay(5000);
+//    closegraph();
+    return 0;
+//g++ -o program main.cpp -lgraph -std=gnu++11
+
 }
 
 
